7a64d6545c8d4a196c19edfd819b547371b06e59
nickarora <arora.nick@gmail.com>
2015-02-08 07:29:12 -0800

    Tweaked CSS for follow buttons on User Page

--- app/assets/javascripts/views/boards/item.js
+++ app/assets/javascripts/views/boards/item.js
@@ -2,15 +2,41 @@ Pinly.Views.BoardsItem = Backbone.CompositeView.extend({
 
 	template: JST['boards/boardsitem'],
 
+	className: 'board-item-wrapper',
+
 	events: {
 		'click .delete': 'removeBoard',
 		'click .edit': 'editBoard',
-		'click': 'showBoard'
+		'click .boards-item': 'showBoard',
+		'click .follow-board-item': 'followHandler'
 	},
 
 	initialize: function(options){
 		this.listenTo(this.model, 'sync change', this.render);
 		this.listenTo(this.model.follows(), 'add remove', this.render);
+	},
+
+	followHandler: function(event){
+		event.preventDefault();
+		var that = this;
+
+		if ( this.model.follows().length ) {
+			var follow = this.model.follows().first();
+			follow.destroy({
+				success: function(){
+					that.model.follows().remove(follow);			
+				}
+			});
+		} else {
+			var follow = new Pinly.Models.Follow();
+			follow.set('user_id', Pinly.CURRENT_USER.id);
+			follow.set('board_id', this.model.id);
+			follow.save({}, {
+				success: function(){
+					that.model.follows().add(follow);
+				}
+			});
+		}
 	},
 
 	showBoard: function(event){
